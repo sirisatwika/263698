601339f58caa4c5148f8cd93f5ffcc1ff5b0605f
sirisatwika <sirisatwikakotha29.com>
2021-04-18 14:25:52 +0530

    main

---
+++ commas.py
@@ -0,0 +1,2 @@
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+# the commas in the print statement would make them 3 seperate strings and while printing the statements we would get a space in betwenn
