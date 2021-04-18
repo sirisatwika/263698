19d42c729549c81b2e6380cbaaba8d9b9777ace0
sirisatwika <sirisatwikakotha29.com>
2021-04-18 14:29:57 +0530

    main

---
+++ printstmt.py
@@ -0,0 +1,10 @@
+arr = [2,3,4,5,6,7,8,9]
+for i in range(0,len(arr)):
+    print(arr[i])
+
+#here the array elements are printed line by line
+
+for i in range(0,len(arr)):
+    print(arr[i],end = " , ")
+
+#here the array elements in the second for are printed with commas seperated.
