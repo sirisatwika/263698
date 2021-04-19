64e35e93efe4a64b0eec04de6e293ca6d8fd6666
sirisatwika <sirisatwikakotha29.com>
2021-04-19 08:00:10 +0530

    main

---
+++ basic_input.py
@@ -0,0 +1,9 @@
+anything = int(input("Enter a number: "))
+something = anything ** 2.0
+print(anything, "to the power of 2 is", something)
+string = input("Enter a string : ")
+print(string)
+len_a = float(input("Input first leg length: "))
+len_b = float(input("Input second leg length: "))
+hypo = (len_a**2 + len_b**2) ** .5
+print("Hypotenuse length is", hypo)
