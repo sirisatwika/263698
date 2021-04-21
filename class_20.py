a5bf8f865e0cad74880bd31a2771da85669255db
sirisatwika <sirisatwikakotha29.com>
2021-04-21 17:51:53 +0530

    main

---
+++ class_20.py
@@ -0,0 +1,18 @@
+"Hello Python"
+print(__doc__)
+a = input()
+b = input()
+res = a + b
+print(res)
+hh,mm,ss = 10,12,25
+print(hh,mm,ss,sep=":")
+x = 10
+y = 2.3
+s1 = "hello"
+ch = 'A'
+flag = True
+c1 = 2 + 3j
+print(type(x))
+print(type(c1))
+print(type(s1))
+print(isinstance(x,int))
