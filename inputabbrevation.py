7da5d36bd0e0a73c90d318e9541e58e92451d707
sirisatwika <sirisatwikakotha29.com>
2021-04-22 22:25:03 +0530

    main

---
+++ inputabbrevation.py
@@ -0,0 +1,12 @@
+s = input("enter a short form : ")
+if s == "lol":
+    print("laughing out loud")
+elif s == "rofl":
+    print("rolling on the floor laughing")
+elif s == "lmk":
+    print("let me know")
+elif s == "smh":
+    print("shaking my head")
+else:
+    print("Please enter a valid expression")
+
