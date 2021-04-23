f564428611b54d73b863c01ed46b2dd761a321ba
sirisatwika <sirisatwikakotha29.com>
2021-04-23 09:52:25 +0530

    main

--- importex.py
+++ importex.py
@@ -1,2 +1,12 @@
 import math
+# The entities dont affect each other
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+pi = 3.14
+print(sin(pi/2))
 print(math.sin(math.pi/2))
+
