615c3670051eed0569167bb98a91827486fb1026
sirisatwika <sirisatwikakotha29.com>
2021-04-23 11:09:51 +0530

    main

---
+++ importexamples.py
@@ -0,0 +1,8 @@
+from math import e, exp, log,sin,cos,tan,asin,sinh,cosh,tanh,pi
+print(pow(e, 1) == exp(log(e)))
+print(pow(2, 2) == exp(2 * log(2)))
+print(log(e, e) == exp(0))
+print(sin(pi/2))
+print(asin(sin(pi)))
+print(sin(pi/2)/cos(pi))
+print(sinh(pi/2))
