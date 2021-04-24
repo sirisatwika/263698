05ab32fa47903791f4a5e6602a790fb528d8fabb
sirisatwika <sirisatwikakotha29.com>
2021-04-24 12:40:47 +0530

    main

--- min_max_string.py
+++ min_max_string.py
@@ -3,3 +3,8 @@ t = 'The Knights Who Say "Ni!"'
 print('[' + min(t) + ']')
 t = [0, 1, 2]
 print(min(t))
+print(max("aAbByYzZ"))
+t = 'The Knights Who Say "Ni!"'
+print('[' + max(t) + ']')
+t = [0, 1, 2]
+print(max(t))
