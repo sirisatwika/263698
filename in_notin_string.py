c6880c9aee048d8f11922bbdc4aca274abdb20fc
sirisatwika <sirisatwikakotha29.com>
2021-04-24 12:36:05 +0530

    main

---
+++ in_notin_string.py
@@ -0,0 +1,13 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+print("f" in alphabet)
+print("F" in alphabet)
+print("1" in alphabet)
+print("ghi" in alphabet)
+print("Xyz" in alphabet)
+print("f" not in alphabet)
+print("F" not in alphabet)
+print("1" not in alphabet)
+print("ghi" not in alphabet)
+print("Xyz" not in alphabet)
+
+
