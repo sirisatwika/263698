92ebc92e2a609e09c7a0dc17af75ae78a028d1a9
sirisatwika <sirisatwikakotha29.com>
2021-04-24 12:30:02 +0530

    main

---
+++ stringindexing.py
@@ -0,0 +1,7 @@
+the_string = 'silly walks'
+for ix in range(len(the_string)):
+    print(the_string[ix], end=' ')
+print()
+the_string = 'silly walks'
+for character in the_string:
+    print(character, end=' ')
