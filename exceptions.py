49efc5351c0cedf8c1c22f87b618704f9fc6ca94
sirisatwika <sirisatwikakotha29.com>
2021-04-25 12:42:52 +0530

    main

---
+++ exceptions.py
@@ -0,0 +1,11 @@
+try:
+    x = int(input("Enter a number: "))
+    y = 1 / x
+    print(y)
+except ZeroDivisionError:
+    print("You cannot divide by zero, sorry.")
+except ValueError:
+    print("You must enter an integer value.")
+except:
+    print("something went wrong...")
+
