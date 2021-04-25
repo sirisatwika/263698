d938d1ea65db175f7503eb5f1aa07ef8b564fc24
sirisatwika <sirisatwikakotha29.com>
2021-04-25 09:20:47 +0530

    main

---
+++ endswith.py
@@ -0,0 +1,10 @@
+if "epsilon".endswith("on"):
+    print("yes")
+else:
+    print("no")
+t = "zeta"
+print(t.endswith("a"))
+print(t.endswith("A"))
+print(t.endswith("et"))
+print(t.endswith("eta"))
+
