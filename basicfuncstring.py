cfe9f52943c3b987f9ca0eea75a7757cc8261e3a
sirisatwika <sirisatwikakotha29.com>
2021-04-25 09:27:21 +0530

    main

---
+++ basicfuncstring.py
@@ -0,0 +1,9 @@
+print('lambda30'.isalnum())
+print('lambda'.isalnum())
+print('30'.isalnum())
+print('@'.isalnum())
+print('lambda_30'.isalnum())
+print(''.isalnum())
+print("Eta".find("ta"))
+print("Eta".find("mma"))
+
